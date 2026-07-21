94da9609e42252334cfd36b553b5700ba94143d6
phochan <cbmja5533@empasy.com>
2026-07-21 10:46:33 +0900

    download link 수정

---
+++ assets/js/downloadLink.js
@@ -0,0 +1,23 @@
+const downloadLink = {
+    "version": "0.0.76",
+    "releaseNote": "",
+    "files": {
+        "winX64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.1.11.exe",
+        "winArm64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.1.11.exe",
+        "macIntel": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA-0.1.11.dmg",
+        "macSilicon": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA-0.1.11-arm64.dmg"
+    }
+}
+
+
+const updateLink = (id, url) => {
+    const el = document.getElementById(id);
+    if (el && url) {
+        el.href = url;
+        el.setAttribute('target', '_blank');
+    }
+};
+updateLink('winArm64', downloadLink.files.winArm64);
+updateLink('winX64', downloadLink.files.winX64);
+updateLink('macIntel', downloadLink.files.macIntel);
+updateLink('macSilicon', downloadLink.files.macSilicon);
