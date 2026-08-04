71c4a7937dc9b441489fe4135461deaa1e190d18
cbmja <143980455+cbmja@users.noreply.github.com>
2026-08-04 13:41:20 +0900

    win - v0.0.17

--- assets/js/downloadLink.js
+++ assets/js/downloadLink.js
@@ -2,8 +2,8 @@ const downloadLink = {
     "version": "0.0.76",
     "releaseNote": "",
     "files": {
-        "winX64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.0.16.exe",
-        "winArm64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.0.16.exe",
+        "winX64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.0.17.exe",
+        "winArm64": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA+Setup+0.0.17.exe",
         "macIntel": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA-0.0.14.dmg",
         "macSilicon": "https://synceta-release.s3.ap-northeast-2.amazonaws.com/SyncETA-0.0.14-arm64.dmg"
     }
