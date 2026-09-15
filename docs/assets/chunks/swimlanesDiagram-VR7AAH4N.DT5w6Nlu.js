858df17fafc8d9d977ee87b671eb7c628d193e51
sanguk12 <sanguk12@users.noreply.github.com>
2026-09-15 04:37:40 +0000

    deploy: 563a1c4d905342e70e5eb75ff0f3c81f54d5ee37

---
+++ docs/assets/chunks/swimlanesDiagram-VR7AAH4N.DT5w6Nlu.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.BFjpRj1d.js";import{_ as a}from"../app.JPwi4eOe.js";import"./chunk-5VM5RSS4.DE3PWmkU.js";import"./chunk-XXDRQBXY.DK5RkeCD.js";import"./chunk-POPQ4Y6H.BI3JUQJe.js";import"./chunk-F27PBJKO.CclBojb0.js";import"./channel.B1LRRVif.js";import"./framework.CTwvqqO-.js";import"./theme.UPELar85.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
