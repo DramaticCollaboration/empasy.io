849fbb9ff7d19557cccba82837bd0335a08de0d7
qudansdl <qudansdl@users.noreply.github.com>
2026-09-15 01:29:53 +0000

    deploy: d40213baa13bae683deb98b49bb51841b2db6cdd

---
+++ docs/assets/chunks/swimlanesDiagram-VR7AAH4N.s0ERZWmW.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.B-VdsJiW.js";import{_ as a}from"../app.DHvhDmQg.js";import"./chunk-5VM5RSS4.HkB5A2RC.js";import"./chunk-XXDRQBXY.Br6Ie9D-.js";import"./chunk-POPQ4Y6H.C4I7k0Lv.js";import"./chunk-F27PBJKO.Dkq4yGjd.js";import"./channel.DsKLBIMl.js";import"./framework.CTwvqqO-.js";import"./theme.DmnToAQV.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
