e4167426f3b946739cb0e615eff4568669d4e8b8
cbmja <cbmja@users.noreply.github.com>
2026-09-16 17:11:58 +0000

    deploy: cd8abed06c9f8691222013576073df0423a51d13

--- docs/assets/chunks/channel.lVQKaLHh.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.Cnua2o71.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/assets/chunks/swimlanesDiagram-VR7AAH4N.CpeMb_ws.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.ColP6gNk.js";import{_ as a}from"../app.NX30aSH7.js";import"./chunk-5VM5RSS4.BhtbckZt.js";import"./chunk-XXDRQBXY.BaadjS2y.js";import"./chunk-POPQ4Y6H.DENAF8wv.js";import"./chunk-F27PBJKO.CbZ1vDlE.js";import"./channel.Ccz3XuYI.js";import"./framework.CTwvqqO-.js";import"./theme.B3rqrlw1.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
