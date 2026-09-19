99df66ea3c1359ca657930fa684cc340772a3f45
cbmja <cbmja@users.noreply.github.com>
2026-09-19 09:04:32 +0000

    deploy: ebd40a21dd042e93bc49038d3938d03925badfde

--- docs/assets/chunks/swimlanesDiagram-VR7AAH4N.Dlyc7dts.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW.Z7Ads6MZ.js";import{_ as a}from"../app.PAXgzX0V.js";import"./chunk-5VM5RSS4.BeA8jCzy.js";import"./chunk-XXDRQBXY.BHKJBuPo.js";import"./chunk-POPQ4Y6H.BBfvcGPH.js";import"./chunk-F27PBJKO.BJVKXZbh.js";import"./channel.BFIOADLF.js";import"./framework.C7mCOqKN.js";import"./theme.DJXCuJc-.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
